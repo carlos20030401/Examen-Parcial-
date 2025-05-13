3a4a12f1d1b78a052b562126f1525ac34abe4830
Carlos Alberto Barriga Arenas <cbarrigaa@ulasalle.edu.pe>
2025-05-13 08:30:58 -0500

    creamos la clase matriz

--- secuencial/matrices-secuencial.cpp
+++ secuencial/matrices-secuencial.cpp
@@ -5,3 +5,12 @@
 
 
 using namespace std;
+
+class Matriz {
+private:
+    int size;
+    vector<vector<int>> datos;
+
+public:
+    Matriz(int n) : size(n), datos(n, vector<int>(n)) {
+        generarAleatorios(
