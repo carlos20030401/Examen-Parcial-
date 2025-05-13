1da0e06a62338936e3c89df4d9ad41e33dd6b214
Carlos Alberto Barriga Arenas <cbarrigaa@ulasalle.edu.pe>
2025-05-13 16:17:31 -0500

    hubo pequeños errores y bueno es nuestro bucle de numeros que incrementara

--- threads/matrices-threads.cpp
+++ threads/matrices-threads.cpp
@@ -4,15 +4,20 @@
 #include <chrono>
 #include <random>
 
+
 using namespace std;
 mutex mtx;  // para proteger el acceso a la suma global
+
 class Matriz {
 private:
     int N;
     vector<vector<int>> datos;
 public:
-    Matriz(int n) : N(n), datos(n, vector<int>()) {
+    Matriz(int n) : N(n), datos(n, vector<int>(n)) {
         random_device rd;
         mt19937 gen(rd());
         uniform_int_distribution<> dis(0, 9);
-        for (int i = 0; i < N ++i)
+        for (int i = 0; i < N; ++i)
+            for (int j = 0; j < N; ++j)
+                datos[i][j] = dis(gen);
+    }
