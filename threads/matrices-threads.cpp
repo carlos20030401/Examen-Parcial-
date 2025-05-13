3db903146d67279ac117611a400fd66fb906f115
Carlos Alberto Barriga Arenas <cbarrigaa@ulasalle.edu.pe>
2025-05-13 16:16:01 -0500

    seguimos con lo que es la clase

--- threads/matrices-threads.cpp
+++ threads/matrices-threads.cpp
@@ -10,3 +10,9 @@ class Matriz {
 private:
     int N;
     vector<vector<int>> datos;
+public:
+    Matriz(int n) : N(n), datos(n, vector<int>()) {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, 9);
+        for (int i = 0; i < N ++i)
