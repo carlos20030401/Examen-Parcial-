84ec10baf9d40fe7ee92b7ee70312e18524e27cf
Carlos Alberto Barriga Arenas <cbarrigaa@ulasalle.edu.pe>
2025-05-13 08:33:30 -0500

    AÑADIMOS la funcion para poder tener numeros aleatorios del 0 al 9

--- secuencial/matrices-secuencial.cpp
+++ secuencial/matrices-secuencial.cpp
@@ -13,4 +13,10 @@ private:
 
 public:
     Matriz(int n) : size(n), datos(n, vector<int>(n)) {
-        generarAleatorios(
+        generarAleatorios();
+        }
+    void generarAleatorios() {
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                datos[i][j] = rand() % 10; // Numeros randoms o aleatorios de 0 a 9
+    }
